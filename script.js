7bad5a60d8f4012c04daf77c8867e4f18cbec285
Emily Florence Starr <emilyflo.starr@gmail.com>
2022-02-16 20:08:14 -0500

    edit is wonky

--- script.js
+++ script.js
@@ -38,6 +38,9 @@ notesOutput.addEventListener('click', function (event) {
     if (event.target.classList.contains('delete')) {
         deleteNote(event.target)
     }
+    if (event.target.classList.contains('edit')) {
+        editNote(event.target)
+    }
 })
 
 function renderNoteItem(noteObj) {
@@ -47,8 +50,15 @@ function renderNoteItem(noteObj) {
     <h2>${noteObj.title}</h2>
     <p>${noteObj.body}</p>
     <span class="material-icons-outlined delete">delete</span>
+    <span class="material-icons edit">edit</span>
     `
     notesOutput.appendChild(noteCard)
+}
+
+function renderNoteText(noteItem, noteObj) {
+    noteItem.innerHTML = `
+    ${todoObj.item}
+    `
 }
 
 function deleteNote(element) {
@@ -58,6 +68,36 @@ function deleteNote(element) {
     }).then(function () {
         element.parentElement.remove()
     })
+}
+
+function editNote(element) {
+    const noteId = element.parentElement.id
+    const noteText = document.querySelector('.edit-note')
+        // const noteTitle = document.querySelector('#note-title').value
+    // const noteBody = document.querySelector('#note-body').value
+    fetch(`http://localhost:3000/notes/${noteId}`, {
+        method: 'PATCH',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            body: noteText,
+        })
+    })
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (data) {
+        console.log(data)
+        renderNoteItem(element.parentElement, data)
+    })
+}
+
+function showEditInput(noteItem) {
+    noteItem.innerHTML =   `
+    <textarea class="edit-text" value="${noteItem.textContent}" autofocus>
+    <button class="update-note" data-note=${noteBody.id}>save</button>
+    <button class="cancel">nevermind</button>
+    `
+    noteItem.querySelector('input').select()
 }
 
 myNotes();
